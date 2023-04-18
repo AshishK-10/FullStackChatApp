1126f7d1f5f9de9e87a6a9df9177759c098359cc
ashish kumar <kumarashish2615@gmail.com>
2023-04-18 19:11:09 +0530

    third commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import {Route} from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Chatpage from './pages/Chatpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Route path = '/' component = {Homepage} exact />
+      <Route path ='/chats' component = {Chatpage}/>
+
+    </div>
+  );
+}
+
+export default App;
